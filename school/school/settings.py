dd3d3c4c1b2e16831ff5b92df14d910a0da2f777
revaldy-30 <rejarevaldy30@gmail.com>
2021-04-23 23:48:31 +0800

    showing image field on home page

--- school/school/settings.py
+++ school/school/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+MEDIA_ROOT = (BASE_DIR/  'images')
+MEDIA_URL = '/images/'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIR = [
